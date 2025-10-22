23a7d88ea761489fc84094a340fbca68e279480e
curbol <curtis.bollinger@gmail.com>
2025-10-22 16:52:56 -0700

    more via space

--- ergogen/footprints/ceoloide/switch_choc_v1_v2.js
+++ ergogen/footprints/ceoloide/switch_choc_v1_v2.js
@@ -515,7 +515,7 @@ module.exports = {
   )
     `
 
-    const via_y_center = -4.5;
+    const via_y_center = -5;
     const from_via_y = via_y_center;
     const to_via_y = via_y_center + p.via_separation;
 
